04eed331c5ca2da9f38b48b891349a3d96f19fd3
BuildTools <unconfigured@null.spigotmc.org>
2018-09-09 17:37:25 -0700

    Ability to create your own wallets

--- TRTL_WPF/Initial.xaml.cs
+++ TRTL_WPF/Initial.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Ini.Net;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using Newtonsoft.Json.Linq;
+using System.Threading;
 
 namespace TRTL_WPF
 {
@@ -20,11 +26,19 @@ namespace TRTL_WPF
     /// </summary>
     public partial class Initial
     {
+        string filepath = "";
         readonly string root = AppDomain.CurrentDomain.BaseDirectory;
-
+        private void Initialsetup()
+        {
+            IniFile ini = new IniFile("tscripta.ini");
+            ini.WriteString("Config", "Filepath", "");
+            ini.WriteString("Config", "Remotedaemonurl", "");
+            ini.WriteBoolean("Config", "Local", false);
+        }
         public Initial()
         {
-            if (!File.Exists(root+@"\TurtleCoind.exe"))
+            if (File.Exists(root + @"\service.log")) File.Delete(root + @"\service.log");
+            if (!File.Exists(root + @"\TurtleCoind.exe"))
             {
                 MessageBox.Show("Turtlecoind.exe does not exist in the local directory, download it from the Turtlecoin repo");
                 this.Close();
@@ -34,7 +48,70 @@ namespace TRTL_WPF
                 MessageBox.Show("turtle-service.exe does not exist in the local directory, download it from the Turtlecoin github repo");
                 this.Close();
             }
+            if (!File.Exists(root + @"\tscripta.ini")) Initialsetup();
             InitializeComponent();
+            Login.Visibility = Visibility.Hidden;
+        }
+        private void CreateWallet(object sender, RoutedEventArgs e)
+        {
+            Walletcreation create = new Walletcreation();
+            if ((bool)create.ShowDialog())
+            {
+                MessageBox.Show(create.Result[0] + create.Result[1]);
+            }
+        }
+
+        private void Openwallet(object sender, RoutedEventArgs e)
+        {
+            Default.Visibility = Visibility.Hidden;
+            Login.Visibility = Visibility.Visible;
+            Transitionframe.Reload();
+        }
+
+        private void Setdirectory(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog diag = new CommonOpenFileDialog
+            {
+                EnsureFileExists = true, DefaultDirectory = root
+            };
+            diag.ShowDialog();
+            try { filepath = diag.FileName; }
+            catch (InvalidOperationException) { filepath = ""; }
+            Pathbox.Text = filepath;
+        }
+
+        private void Openwalletfile(object sender, RoutedEventArgs e)
+        {
+            walletloading.IsActive = true;
+            string password = passwordloginbox.Password;
+            string path = filepath;
+            Thread Thread = new Thread(() =>
+            {
+                Process Process = new Process();
+                Process.StartInfo.FileName = root + @"\turtle-service.exe";
+                Process.StartInfo.Arguments = "-w " + path + " -p " + password + " --rpc-legacy-security";
+                Process.StartInfo.UseShellExecute = false;
+                Process.StartInfo.CreateNoWindow = true;
+                if (Process.Start())
+                {
+                    // Begin redirecting output
+                    Thread.Sleep(4750);
+                    if (!Process.HasExited) Process.Kill();
+                    Thread.Sleep(200);
+                    string contents = File.ReadAllText(root + @"\service.log");
+                    if (contents.Contains("Restored view public key doesn't correspond to secret key: The password is wrong"))
+                    {
+                        Application.Current.Dispatcher.Invoke(new Action(() => { MessageBox.Show("Password is Incorrect"); walletloading.IsActive = false; }));
+                    }
+                    else
+                    {
+                        Application.Current.Dispatcher.Invoke(new Action(() => { MessageBox.Show("Password is Correct"); walletloading.IsActive = false; }));
+                    }
+                }
+            })
+            { IsBackground = true };
+            Thread.SetApartmentState(ApartmentState.STA);
+            Thread.Start();
         }
     }
 }
---
+++ TRTL_WPF/Walletcreation.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Diagnostics;
+namespace TRTL_WPF
+{
+    /// <summary>
+    /// Interaction logic for Walletcreation.xaml
+    /// </summary>
+    public partial class Walletcreation
+    {
+        readonly string root = AppDomain.CurrentDomain.BaseDirectory;
+        public Walletcreation()
+        {
+            InitializeComponent();
+        }
+        public string[] Result { get; private set; }
+
+        private void Create_wallet(object sender, RoutedEventArgs e)
+        {
+            string password = Pw2.Password;
+            if (Pw1.Password == Pw2.Password)
+            {
+                if (Pw1.Password.Length <= 5)
+                {
+                    MessageBox.Show("Password is Too Short! (Must be greater than 5");
+                }
+
+                else
+                {
+                    if (File.Exists(root + @"\" + Filename.Text + ".wallet"))
+                    {
+                        MessageBox.Show("File name already exists!");
+                    }
+                    else
+                    {
+                        // Create wallet process
+                        Process Process = new Process();
+                        Process.StartInfo.FileName = root + @"\turtle-service.exe";
+                        Process.StartInfo.Arguments = "--g -w " + Filename.Text + ".wallet -p " + password; 
+                        Process.StartInfo.UseShellExecute = false;
+                        Process.StartInfo.RedirectStandardInput = true;
+                        Process.StartInfo.RedirectStandardOutput = true;
+                        Process.StartInfo.RedirectStandardError = true;
+                        Process.StartInfo.CreateNoWindow = true;
+                        // Start process
+                        if (Process.Start())
+                        {
+                            Process.WaitForExit();
+                            string[] res = { Filename.Text, Pw1.Password };
+                            Result = res;
+                            this.DialogResult = true;
+                            this.Close();
+                        }
+                        else { MessageBox.Show("Something went wrong"); }
+                    }
+                
+                }
+            }
+            else { MessageBox.Show("Passwords do not match!"); }
+        }
+
+        private void pwintercept(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space) e.Handled = true;
+        }
+        private void textBox_PreviewExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (e.Command == ApplicationCommands.Copy ||
+                e.Command == ApplicationCommands.Cut ||
+                e.Command == ApplicationCommands.Paste)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
